b27300fbc8cad17fbdfa8bdb7b3c955a03754e96
Glitch (glitch-blank-website) <none>
2021-12-12 21:16:31 +0000

    👚😹 Checkpoint ./script.js:35586298/6998 ./index.html:35586298/28 ./style.css:35586298/59

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
-// my function is to rotate the font colors of the poems
-
-
-// We'll create two constants
-const sectionTag = document.querySelector("section");
+const imageArea = document.querySelector(".container");
 const button = document.querySelector("button");
 
-button.onclick =12function(){"section"};
-button.addEventListener("click",function() {
+button.addEventListener("click", function() {
+  imageArea.classList.toggle("h1");
 
-  sectionTag.classList.toggle("new-class");
-
-})
-
-
-
-
-// we set up 3 constants
-// 1. the ticker wrapper (the area where the images are scrolling)
-// 2. the list of items
-// 3. the copy (clone of our list)
-const ticker = document.querySelector('.ticker')
-const list = document.querySelector('.ticker__list')
-const clone = list.cloneNode(true)
-
-// then we keep appending our list inside the ticker
-ticker.append(clone)
-
-
-
-//NICOLE DON'T WORRY I WILL HAVE THIS IN GITHUB!
-
+  if (imageArea.classList.contains("h1")) {
+    button.innerHTML = "REMIX";
+  } else {
+    button.innerHTML = "AGAIN";
+  }
+});
