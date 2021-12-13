f1b72e0917ac519493875c5b9a0dfb097b482725
Glitch (glitch-blank-website) <none>
2021-12-13 20:53:14 +0000

    🚴🚗 Checkpoint ./script.js:35586298/5819 ./index.html:35586298/5606 ./style.css:35586298/6682 ./alternate.css:35586298/6060

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const imageArea = document.querySelector(".column-wrapper");
 const button = document.querySelector("button");
 
 button.addEventListener("click", function() {
-  imageArea.classList.toggle("grid");
+  imageArea.classList.toggle("h1");
 
-  if (imageArea.classList.contains("grid")) {
+  if (imageArea.classList.contains("h1")) {
     button.innerHTML = "DO SOMETHING";
   } else {
     button.innerHTML = "DO NOTHING";
