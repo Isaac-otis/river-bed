52ecad81d5858117c6011bccc27d193ee4c20df8
Glitch (glitch-blank-website) <none>
2021-12-13 20:42:08 +0000

    🌒💽 Checkpoint ./script.js:35586298/4196 ./index.html:35586298/36356

--- script.js
+++ script.js
@@ -10,10 +10,3 @@ button.addEventListener("click", function() {
     button.innerHTML = "DO NOTHING";
   }
 });
-
-function themeSwitch() {
-    var t1 = document.getElementById('theme');
-    var t2 = document.getElementById('alttheme');
-    t1.disabled = !t1.disabled;
-    t2.disabled = !t1.disabled;
-}
